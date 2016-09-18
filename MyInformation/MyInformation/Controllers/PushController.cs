f0ebddb524bdaab3cd737802acfbfa78f6a15478
JainChanchal <coolcanchal108@gmail.com>
2016-09-18 14:12:35 +0530

    Added a PushController

---
+++ MyInformation/MyInformation/Controllers/PushController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyInformation.Controllers
+{
+    public class PushController : Controller
+    {
+        //
+        // GET: /Push/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
